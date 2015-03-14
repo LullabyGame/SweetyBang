9e8392e6a1e6e147034461629d5313a6312431db
wangfang <murreywang@163.com>
2015-03-14 12:00:23 +0800

    修改了： 关卡选择的图标位置，读取json

--- Classes/LevelSelectContent.cpp
+++ Classes/LevelSelectContent.cpp
@@ -25,21 +25,60 @@ bool LevelSelectContent::init() {
 void LevelSelectContent::initAllLevels(ssize_t idx) {
 //    this->removeAllChildren();
     int idxs = (int) idx; 
-    //假设每页10关
+    /*假设每页10关*/
     int firstNumber = idxs * 10 + 1;
     
-    for (int i = 0; i < 10; i++ ) {
-        levelMenu = Menu::create();
-        this->addChild(levelMenu);
-        
-        auto oneLevelItem = LevelSelectItem::create(firstNumber + i);
-        levelMenu->addChild(oneLevelItem);
-        oneLevelItem->setPosition(350, 150 + i * 100);
-        oneLevelItem->setScale(itemScale, itemScale);
-        
-        levelMenu->setPosition(0, 0);
-        levelMenu->setOpacity(0);
-        auto fadeAction = FadeIn::create(0.5);
-        levelMenu->runAction(fadeAction);
+    /* json读取 */
+    std::string stageName = "res/stage/LevelPosition.json";
+    
+    if(!FileUtils::getInstance()->isFileExist(stageName)) {
+        CCLOG("json file is not find");
+        return;
     }
+    
+    std::string data = FileUtils::getInstance()->getStringFromFile(stageName);
+    rapidjson::Document doc;
+    doc.Parse<rapidjson::kParseDefaultFlags>(data.c_str());
+    rapidjson::Value& tileInfo = doc["levels1"];
+    if (tileInfo.IsArray()) {
+        for (int row = 0; row < tileInfo.Size(); row++) {
+            rapidjson::Value& cols = tileInfo[row];
+            rapidjson::Value &positionX = cols["PositionX"];
+            rapidjson::Value &positionY = cols["PositionY"];
+            int posX = positionX.GetInt();
+            int posY = positionY.GetInt();
+            log("PositionX: %i",posX);
+            log("PositionY: %i",posY);
+            log("row: %i",row);
+            
+            levelMenu = Menu::create();
+            this->addChild(levelMenu);
+            
+            auto oneLevelItem = LevelSelectItem::create(firstNumber + row);
+            levelMenu->addChild(oneLevelItem);
+            oneLevelItem->setPosition(posX, posY);
+            oneLevelItem->setScale(itemScale, itemScale);
+            
+            levelMenu->setPosition(0, 0);
+            levelMenu->setOpacity(0);
+            auto fadeAction = FadeIn::create(0.5);
+            levelMenu->runAction(fadeAction);
+        }
+    }
+
+    
+//    for (int i = 0; i < 10; i++ ) {
+//        levelMenu = Menu::create();
+//        this->addChild(levelMenu);
+//        
+//        auto oneLevelItem = LevelSelectItem::create(firstNumber + i);
+//        levelMenu->addChild(oneLevelItem);
+//        oneLevelItem->setPosition(350, 150 + i * 100);
+//        oneLevelItem->setScale(itemScale, itemScale);
+//        
+//        levelMenu->setPosition(0, 0);
+//        levelMenu->setOpacity(0);
+//        auto fadeAction = FadeIn::create(0.5);
+//        levelMenu->runAction(fadeAction);
+//    }
 }
